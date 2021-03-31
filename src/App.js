4e1db69e23ada8f52fa64debfe1a7737b75aa78b
jeancwes <jeancwes@gmail.com>
2021-03-30 23:20:55 -0300

    Added tests config. Added router config. Added redux config.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route path="/comics">
+          <h1>Marvel</h1>
+        </Route>
+        <Redirect exact from="/" to="/comics" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import "@testing-library/jest-dom/extend-expect";
+
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders marvel heading', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  const heading = screen.getByRole('heading');
+
+  expect(heading).not.toBeNull();
+  expect(heading).toHaveTextContent('Marvel');
 });
