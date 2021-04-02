6cc134774e99917b350bcd432e3f8a0270168d9d
jeancwes <jeancwes@gmail.com>
2021-04-01 21:34:23 -0300

    Refactored code.

--- src/App.js
+++ src/App.js
@@ -1,18 +1,10 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
 
 import Comics from './components/comics/Comics';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Switch>
-        <Route path="/comics">
-          <Comics />
-        </Route>
-        <Redirect exact from="/" to="/comics" />
-      </Switch>
-    </BrowserRouter>
+    <Comics />
   );
 }
 
